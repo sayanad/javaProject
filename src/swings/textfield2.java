3fdc1e6ac5c44e2b05d1e67d45634dea19d74ed5
sayan <babanadhikary490sa@gmail.com>
2022-04-16 10:03:48 -0700

    my awt project

--- src/swings/textfield2.java
+++ src/swings/textfield2.java
@@ -4,13 +4,12 @@ import java.awt.event.*;
 public class textfield2 extends Frame implements ActionListener {
     TextField tf1,tf2,tf3;
     Button b1,b2;
-    Label l;
+    Label l2;
     textfield2(){
-         l=new Label("enter 1st number");
-        l.setBounds(50, 100, 100, 30);
+
 
         tf1=new TextField();
-        tf1.setBounds(50, 50, 150, 20);
+        tf1.setBounds(50, 50, 250, 20);
         tf2=new TextField();
         tf2.setBounds(50, 100, 150, 20);
         tf3=new TextField();
@@ -21,10 +20,13 @@ public class textfield2 extends Frame implements ActionListener {
         b2.setBounds(120,200,50,50);
         b1.addActionListener(this);
         b2.addActionListener(this);
-        add(tf1);add(tf2);add(tf3);add(b1);add(b2);add(l);
+        add(tf1);add(tf2);add(tf3);add(b1);add(b2);
         setSize(400,400);
         setLayout(null);
         setVisible(true);
+
+
+
 
 
 
